934fcbc13f036666236fac9a7ce6e1da8d774dd0
ojun9 <eu.bkw7jdcpa2r@gmail.com>
2023-09-24 18:43:44 +0900

    add test

--- Tests/SwiftTestingTests/SwiftTestingTests.swift
+++ Tests/SwiftTestingTests/SwiftTestingTests.swift
@@ -2,9 +2,41 @@ import XCTest
 import Testing
 @testable import SwiftTesting
 
+
+// グローバルメソッドでもOK
+@Test
+func isHoge() {
+    #expect(2 == 2)
+}
+
 struct SwiftTestingTests2 {
     @Test
     func isHoge() {
         #expect(2 == 2)
+    }
+    
+    // @Testがないと評価されない
+    func isFuga() {
+        #expect(4 == 3)
+    }
+    
+    // MainThreadでは動かない
+    // XCTestは全てメインスレッドで動いてた
+    @Test()
+    func notMainThread() {
+        #expect(!Thread.isMainThread)
+    }
+    
+    // MainThreadで動かすには以下のようにする
+    @Test @MainActor
+    func thisIsMainThread1() {
+        #expect(Thread.isMainThread)
+    }
+    
+    @Test
+    func thisIsMainThread2() {
+        Task { @MainActor
+            #expect(Thread.isMainThread)
+        }
     }
 }
